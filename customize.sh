bca9f915a7508d9ecf4023f944a0a6f5b6efb0c8
akari263 <30289445+akari263@users.noreply.github.com>
2024-06-09 00:26:42 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,4 @@ sed -i 's/192.168.1.1/192.168.0.4/g' openwrt/package/base-files/files/bin/config
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
 #sed -i 's/root:.*/root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::/g' package/base-files/files/etc/shadow
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
